2f6efa8221a411df0969cfe6a2129907096bfd4d
muriukialex-n <muriukialex.n@gmail.com>
2024-01-25 20:54:48 +0000

    alex,0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,34 +1,13 @@
-fest to optimize Nginx configuration for high concurrency
+ncreases the amount of traffic an Nginx server can handle.
 
-# Increase the ULIMIT for Nginx connections
-exec { 'increase-nginx-ulimit':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx && service nginx restart',
-  path    => '/usr/bin:/bin',
-}
-
-# Configure Nginx to handle high concurrency
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => '
-    worker_processes auto;
-    worker_connections 2000;
-    events {
-      multi_accept on;
-      use epoll;
-    }
-  ',
-  notify  => Exec['nginx-restart'],
-}
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
 # Restart Nginx
 exec { 'nginx-restart':
-  command => 'service nginx restart',
-  path    => '/etc/init.d/',
-  refreshonly => true,
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
-
-# Display a notice about the changes
-notify { 'nginx-optimization-notice':
-  message => 'Nginx configuration optimized for high concurrency.',
-}
-
