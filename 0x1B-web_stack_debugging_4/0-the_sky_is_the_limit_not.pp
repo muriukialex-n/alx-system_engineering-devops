14b3cdbf818f5021ae6c484d704f3ec0647dc323
muriukialex-n <muriukialex.n@gmail.com>
2024-01-25 20:46:51 +0000

    aria

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,13 @@
-exec { 'increase-nginx-connections':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx && service nginx restart',
-  path    => '/usr/bin:/bin',
+# Increases the amount of traffic an Nginx server can handle.
+
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
